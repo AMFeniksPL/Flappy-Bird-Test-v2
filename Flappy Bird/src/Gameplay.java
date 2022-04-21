6da98785b14bc07c07813ca119971b121909f1e0
AMFeniksPL <arkadiuszmazurek13@gmail.com>
2022-04-21 02:39:31 +0200

    Added first code to my project

---
+++ Flappy Bird/src/Gameplay.java
@@ -0,0 +1,50 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Gameplay extends JPanel implements KeyListener, ActionListener {
+
+    private int x = 300;
+    private int y = 300;
+    public Gameplay(){
+        addKeyListener(this);
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
+
+        int delay = 8;
+        Timer timer = new Timer(delay, this);
+        timer.start();
+    }
+
+    public void paint(Graphics g){
+        g.setColor(new Color(2, 126, 124));
+        g.fillRect(0, 0, 400, 700);
+        g.setColor(Color.yellow);
+        g.fillRect(x, y, 20, 20);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        repaint();
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_SPACE){
+            y -= 5;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+}

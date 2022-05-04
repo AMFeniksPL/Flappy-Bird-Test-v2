ada30048865bc93127cdbd287ab0967f7c8871ec
AMFeniksPL <arkadiuszmazurek13@gmail.com>
2022-05-04 14:00:14 +0200

    Added Pipe Mechanics

--- Flappy Bird/src/Gameplay.java
+++ Flappy Bird/src/Gameplay.java
@@ -4,14 +4,22 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.LinkedList;
 
 public class Gameplay extends JPanel implements KeyListener, ActionListener {
     //TEST
     //Zmienne przechowujące współrzędne Birda
+    Timer timer;
     private Bird bird = new Bird(150, 300);
+
 
     //Zmienna sterującąca mechanizmem pauzy w grze;
     private boolean isPlaying = false;
+
+    private LinkedList<Pipe> pipeList = new LinkedList<>();
+    private Stopwatch pipeSpawnDelayer = new Stopwatch();
+    private int limit = 3;
+
 
     //Konstruktor klasy Gameplay
     public Gameplay(){
@@ -20,8 +28,9 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
         setFocusTraversalKeysEnabled(false);
 
         int delay = 8;
-        Timer timer = new Timer(delay, this);
+        this.timer = new Timer(delay, this);
         timer.start();
+        pipeSpawnDelayer.start();
     }
 
     //Funkcja rysująca grafikę;
@@ -34,16 +43,28 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
             g.setColor(Color.red);
             g.fillRect(100, 100, 200, 200);
         }
+
+        for (Pipe pipe: pipeList){
+            pipe.draw(g);
+        }
         bird.draw(g);
 
     }
 
     //Główna pętla gry
-    //aaaaa
     @Override
     public void actionPerformed(ActionEvent e) {
         if (isPlaying){
             bird.move();
+
+            if ((int)pipeSpawnDelayer.getElapsedTimeSeconds() >= limit){
+                pipeList.add(new Pipe(500, -100));
+                pipeList.add(new Pipe(500, 400));
+                pipeSpawnDelayer.start();
+            }
+            for (Pipe pipe: pipeList){
+                pipe.move();
+            }
         }
         repaint();
     }
---
+++ Flappy Bird/src/Stopwatch.java
@@ -0,0 +1,11 @@
+public class Stopwatch {
+    private long startTime;
+
+    public void start() {
+        startTime = System.currentTimeMillis();
+    }
+
+    public float getElapsedTimeSeconds() {
+        return (System.currentTimeMillis() - startTime) / 1000f;
+    }
+}
